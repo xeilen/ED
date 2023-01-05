283368fcdd3a42e9622008a9b25d8b579cbc992f
Nazar Kovalenko <xeilen1112@gmail.com>
2023-01-06 00:01:48 +0200

    base URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,8 +3,8 @@ import { getAuthHeaders } from '@/settings/authHeaders'
 
 export const useApi = () =>
   http.create({
-    // baseURL: import.meta.env.VITE_API_URL,
-    baseURL: 'https://ed--api22.herokuapp.com',
+    baseURL: import.meta.env.VITE_API_URL,
+    // baseURL: 'https://ed--api22.herokuapp.com',
     withCredentials: true,
     headers: {
       'Access-Control-Allow-Origin': '*',
